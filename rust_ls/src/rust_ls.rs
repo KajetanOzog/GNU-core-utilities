4578a344b3891e968944f868eb89a373a89ea208
Kajetan <kajetanozog@gmail.com>
2024-02-19 21:38:34 +0100

    ls ver.2

--- rust_ls/src/rust_ls.rs
+++ rust_ls/src/rust_ls.rs
@@ -1,7 +1,16 @@
-use std::fs;
+use std::{env, fs};
+use std::path::Path;
 
-fn main() {
-    let it = fs::read_dir(".").unwrap();
+fn main()
+{
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2
+    {
+        println!("Chryste Panie blad");
+        std::process::exit(1);
+    }
+    let path = &args[1];
+    let it = fs::read_dir(Path::new(&path)).unwrap();
     for i in it
     {
         println!("{}", i.unwrap().file_name().into_string().unwrap());
