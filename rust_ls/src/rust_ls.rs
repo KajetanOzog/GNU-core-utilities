4731ca58b8edd1473dc81d2860f19c549b82947f
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2024-02-20 22:32:07 +0100

    Dodano poczatek obslugi switchow

--- rust_ls/src/rust_ls.rs
+++ rust_ls/src/rust_ls.rs
@@ -4,8 +4,40 @@ use std::path::Path;
 fn main()
 {
     let args: Vec<String> = env::args().collect();
-    let (switch_l, switch_r, switch_a, switch_h) : (bool, bool, bool, bool);
+    let (mut switch_l, mut switch_r, mut switch_a, mut switch_h, mut switch_sort_n, mut switch_sort_s, mut switch_sort_t, mut switch_sort_v, mut switch_sort_x) : (bool, bool, bool, bool, bool, bool, bool, bool, bool);
     let path : String;
+
+    for arg in args{
+        if arg == "-a" || arg == "--all"{
+            switch_a = true;
+        }
+        else if arg == "-l"{
+            switch_l = true;
+        }
+        else if arg == "-r" || arg == "--reverse"{
+            switch_r = true;
+        }
+        else if arg == "-a" || arg == "--all"{
+            switch_h = true;
+        }
+        else if arg == "-U" || arg == "--sort=none"{
+            switch_sort_n = true;
+        }
+        else if arg == "-S" || arg == "--sort=size"{
+            switch_sort_s = true;
+        }
+        else if arg == "-t" || arg == "--sort=time"{
+            switch_sort_t = true;
+        }
+        else if arg == "-v" || arg == "--sort=version"{
+            switch_sort_v = true;
+        }
+        else if arg == "-X" || arg == "--sort=extension"{
+            switch_sort_x = true;
+        }
+    }
+
+
     if args.len() < 2
     {
 
