8fcd27edac93d6449bcb7182119a9c07cf525843
KRZYSZTOF_KULKA <krzysztof.kulka1234@gmail.com>
2024-02-22 13:59:54 +0100

    Basic -a functionality: lacks of option posibbility

--- rust_ls/src/rust_ls.rs
+++ rust_ls/src/rust_ls.rs
@@ -1,5 +1,15 @@
 use std::{env, fs};
+use std::fs::{DirEntry};
 use std::path::Path;
+use std::vec::Vec;
+
+fn is_not_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s|!s.starts_with("."))
+        .unwrap_or(false)
+}
 
 fn main()
 {
@@ -17,16 +27,39 @@ fn main()
         Ok(directories) => directories,
         Err(_) => panic!("Path doesn't exist")
     };
-    for i in it
-    {
-        let dir_entry = match i{
-            Ok(dir) => dir,
-            Err(_) => panic!("Directory name error")
+    // WalkDir::new(Path::new(&path))
+    //     .into_iter()
+    //     .filter_entry(|e| !is_not_hidden(e))
+    //     .filter_map(|v| v.ok())
+    //     .for_each(|x| println!("{:?}", x.file_name().to_os_string()));
+    let mut vec_of_dir: Vec<DirEntry>=Vec::new();
+    for i in it {
+        let dir_entry=match i  {
+            Ok(dir)=>dir,
+            Err(_)=>panic!("Error in file")
         };
-        println!("{}", match dir_entry.file_name().into_string(){
-            Ok(string) => string,
-            Err(_) => panic!("Directory name couldn't be converted into string")
-        });
+        if is_not_hidden(&dir_entry)
+        {
+            vec_of_dir.push(dir_entry);
+        }
     }
+    for dir in vec_of_dir{
+        println!("{}", match dir.file_name().into_string(){
+                     Ok(string) => string,
+                     Err(_) => panic!("Directory name couldn't be converted into string")
+                 })
+    }
+
+    // for i in it
+    // {
+    //     let dir_entry = match i{
+    //         Ok(dir) => dir,
+    //         Err(_) => panic!("Directory name error")
+    //     };
+    //     println!("{}", match dir_entry.file_name().into_string(){
+    //         Ok(string) => string,
+    //         Err(_) => panic!("Directory name couldn't be converted into string")
+    //     })
+    // }
 }
 
