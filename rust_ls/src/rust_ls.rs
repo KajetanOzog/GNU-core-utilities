4a6fcf173ab3e217b1acbe725fb15fd5e04a2533
KRZYSZTOF_KULKA <krzysztof.kulka1234@gmail.com>
2024-02-22 23:05:52 +0100

    added printvecargs

--- rust_ls/src/rust_ls.rs
+++ rust_ls/src/rust_ls.rs
@@ -2,6 +2,7 @@ use std::{env, fs};
 use std::fs::{DirEntry};
 use std::path::Path;
 use std::vec::Vec;
+use std::collections::HashSet;
 
 fn is_not_hidden(entry: &DirEntry) -> bool {
     entry
@@ -10,7 +11,37 @@ fn is_not_hidden(entry: &DirEntry) -> bool {
         .map(|s|!s.starts_with("."))
         .unwrap_or(false)
 }
+fn print_vec_args(vec_of_dir: Vec<DirEntry>, set_of_switches:HashSet<&str> ) ->() {
+    if set_of_switches.contains("switch_sort_n")
+    {
 
+    } else if set_of_switches.contains("switch_sort_s") {
+
+    }
+    else if set_of_switches.contains("switch_sort_t") {
+
+    }
+    else if set_of_switches.contains("switch_sort_v") {
+
+    }
+    else if set_of_switches.contains("switch_sort_x") {
+
+    }
+    else if set_of_switches.contains("switch_sort_reverse") {
+
+    }
+    for dir in vec_of_dir{
+        //switch l w forze
+        //recursive a mianie
+        //human read w forze
+        if set_of_switches.contains("switch_l"){
+
+        }
+        else if set_of_switches.contains("switch_h"){
+
+        }
+    }
+}
 
 fn ls_r(path: String, tabs_nr: u8)
 {
@@ -59,6 +90,7 @@ fn main()
     let (mut switch_l, mut switch_recursive, mut switch_a, mut switch_h, mut switch_sort_n, mut switch_sort_s, mut switch_sort_t, mut switch_sort_v, mut switch_sort_x, mut switch_sort_reverse):
         (bool, bool, bool, bool, bool, bool, bool, bool, bool, bool);
     let mut paths: Vec<String> = Vec::new();
+    let mut switches: HashSet<&str>=HashSet::new();
 
     for arg in args {
         if arg == "-a" || arg == "--all" {
